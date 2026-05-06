3cb765e71da1d5bd5d4b95a0fd09f703dda92991
anchal.koshta007@gmail.com <genious@genious.genious>
2026-05-06 09:10:17 +0530

    My MiddleWare Routing: User- Guest

--- app.js
+++ app.js
@@ -2,14 +2,49 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+let users = [
+{   id : 1, name : "Anchal"},
+{   id : 2, name : "Harish"},
+];
+
+
 app.use((req, res, next)=>{
-    console.log("MiddleWare1 started");
+    req.user = "Guest";
     next();
-}) 
-app.use((req, res, next)=> {
-    console.log('MiddleWare 2 started');
-    res.send('<h1> Ram Ram </h1>');
+})
+app.get("/users", (req, res)=>{
+    res.json(users);
 })
 
+
+app.post("/users", (req, res)=>{
+    const {name} = req.body;
+    const newUser = {id: users.length+1, name};
+
+    users.push(newUser);
+
+    res.status(201).json(newUser);
+})
+
+
+
+// Get Use by ID
+
+app.get("/users/:id", (req, res)=>{
+    const userID = parseInt(req.params.id); // to get ID from the URL
+    const user = users.find(u => u.id === userID);
+
+    if(!user){
+        return res.status(404).json({message: " User Not Found"});
+    }
+
+    res.json(user);
+})
+
+app.use("/welcome", (req, res, next)=>{
+    res.send(`<h1> Welcome , ${req.user}!</h1>`);
+})
 const port = 3000;
-app.listen(port, ()=> console.log(`Server is up and running on port ${port}! Ready to handle requests.`));
+app.listen(port, ()=> console.log(`Server is up and running on port http://localhost:${port}! Ready to handle requests.`));
