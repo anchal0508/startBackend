299e825cc98fef346b481ef05f29f80b1c1c0170
anchal.koshta007@gmail.com <genious@genious.genious>
2026-05-07 14:15:48 +0530

    Get Request with HTML form

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const productRoute = require('./routes/productRoutes');
 const app = express();
 app.use(express.json());
-app.use(productRoute.route);
+app.use("/api",productRoute.route);
 
 app.listen(3000, ()=> console.log('Online...'));
